6e2d24168f9205b25da3b892d0b338047e354451
Seth Bro <seth@sethbro.com>
2013-02-09 13:48:28 -0600

    Add alias for ohmyzsh dir

--- custom/init.zsh
+++ custom/init.zsh
@@ -3,7 +3,6 @@
 INCLUDES=(paths colors hosts git aliases)
 for f in $INCLUDES
 do
-  echo $ZSH_CUSTOM/common/$f
   [[ -f $ZSH_CUSTOM/common/$f ]] && . $ZSH_CUSTOM/common/$f
   [[ -f $ZSH_CUSTOM/$myhost/$f ]] && . $ZSH_CUSTOM/$myhost/$f
 done
