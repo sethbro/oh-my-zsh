d5ba41128a1e4b7797a070e95f58430a3530f45d
Seth Bro <seth@sethbro.com>
2013-03-26 14:44:58 -0700

    Remove nuget script

--- custom/init.zsh
+++ custom/init.zsh
@@ -1,11 +1,13 @@
 . ~/.myhost
 
-INCLUDES=(paths colors hosts git keys aliases)
+INCLUDES=(paths env colors hosts git keys aliases)
 for f in $INCLUDES
 do
   [[ -f $ZSH_CUSTOM/common/$f ]] && . $ZSH_CUSTOM/common/$f
   [[ -f $ZSH_CUSTOM/$myhost/$f ]] && . $ZSH_CUSTOM/$myhost/$f
 done
+
+export PATH
 
 cd $d_www
 
