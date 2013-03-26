d768df81b1d060ea0f4981fb4271b961c69dd3c5
Seth Bro <seth@sethbro.com>
2013-03-26 11:16:43 -0700

    New aliases, etc

--- custom/init.zsh
+++ custom/init.zsh
@@ -1,9 +1,11 @@
 . ~/.myhost
 
-INCLUDES=(paths colors hosts git aliases)
+INCLUDES=(paths colors hosts git keys aliases)
 for f in $INCLUDES
 do
   [[ -f $ZSH_CUSTOM/common/$f ]] && . $ZSH_CUSTOM/common/$f
   [[ -f $ZSH_CUSTOM/$myhost/$f ]] && . $ZSH_CUSTOM/$myhost/$f
 done
+
+cd $d_www
 
